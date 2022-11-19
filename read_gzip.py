7ea31c59ee5800817d4ed27da517ae3ddd7a2ef6
pisa <pisa0401mst@gmail.com>
2022-11-20 05:13:32 +0900

    gzip data to var

--- read_gzip.py
+++ read_gzip.py
@@ -3,3 +3,7 @@ import gzip
 with gzip.open("data/foo.csv.gz", "rb") as f:
     for buff in f.readlines():
         print(buff)
+
+with gzip.open("data/foo.csv.gz", "rb") as f:
+    data = f.read()
+    print(data)
